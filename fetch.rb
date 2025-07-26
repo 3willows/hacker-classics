9acdbe64c913aabda9e8c376bd120bd8eaf1a304
Jasper Wong <wlcjasper@gmail.com>
2025-07-26 20:36:28 +0100

    Change criterion

--- fetch.rb
+++ fetch.rb
@@ -5,18 +5,20 @@ require 'json'
 require 'pry'
 
 POINT_THRESHOLD = 40
+EXCLUDE_KEYWORDS = /\b(engineer|tech|software|programming)\b/i  # Matches whole words like 'engineer' or 'tech', case-insensitive
 
 stories = Hash.new { |h, k| h[k] = [] }
 
 ["philosophy", "history", "literature"].each do |topic|
-  (0..2).each do |page|
+  (0..100).each do |page|
     ct = 0
     puts "Fetch #{"https://hn.algolia.com/api/v1/search?tags=story&query=#{topic}&page=#{page}"}..."
     html = URI.parse("https://hn.algolia.com/api/v1/search?tags=story&query=#{topic}&page=#{page}").read
     data = JSON.parse(html)
   
     data['hits'].each do |h|
-      if h['points'] > POINT_THRESHOLD
+      title = h['title'] || ""
+      if h['points'] > POINT_THRESHOLD && !title.match?(EXCLUDE_KEYWORDS)
         puts "  #{h['title']} -- #{h['points']}"
         stories[topic] << h
         ct += 1
