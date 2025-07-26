1b96decfbd8bed7740f2295e14c58cc0d5d27ed9
Jasper Wong <wlcjasper@gmail.com>
2025-07-26 19:59:26 +0100

    Change name for clarity

--- fetch.rb
+++ fetch.rb
@@ -8,17 +8,17 @@ POINT_THRESHOLD = 40
 
 stories = Hash.new { |h, k| h[k] = [] }
 
-["philosophy", "history", "literature"].each do |year|
+["philosophy", "history", "literature"].each do |topic|
   (0..2).each do |page|
     ct = 0
-    puts "Fetch #{"https://hn.algolia.com/api/v1/search?tags=story&query=#{year}&page=#{page}"}..."
-    html = URI.parse("https://hn.algolia.com/api/v1/search?tags=story&query=#{year}&page=#{page}").read
+    puts "Fetch #{"https://hn.algolia.com/api/v1/search?tags=story&query=#{topic}&page=#{page}"}..."
+    html = URI.parse("https://hn.algolia.com/api/v1/search?tags=story&query=#{topic}&page=#{page}").read
     data = JSON.parse(html)
   
     data['hits'].each do |h|
       if h['points'] > POINT_THRESHOLD
         puts "  #{h['title']} -- #{h['points']}"
-        stories[year] << h
+        stories[topic] << h
         ct += 1
       end
     end
